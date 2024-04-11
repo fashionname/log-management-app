f29775d4e24b62a13c43021f8f96cff871f2c77b
admin <1244107044@qq.com>
2024-04-11 12:29:13 +0800

    commit init

---
+++ src/main/java/com/cowbaby/logmanagementapp/LogManagementAppApplication.java
@@ -0,0 +1,13 @@
+package com.cowbaby.logmanagementapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogManagementAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogManagementAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cowbaby/logmanagementapp/LogManagementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cowbaby.logmanagementapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogManagementAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
